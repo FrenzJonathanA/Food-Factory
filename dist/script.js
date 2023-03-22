15b22bd77ed4a7eab015457b391798c755927d00
unknown <popsgie>
2023-03-22 15:53:11 +0800

    slider updated

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,19 @@
+const headerB = document.querySelector(".header");
+window.addEventListener("scroll", () => {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset >= 100){
+    headerB.classList.add("shadowB")
+  }else{
+    headerB.classList.remove("shadowB");
+  }
+})
+
+
 const toggleMenu = document.querySelector(".toggle__menu");
 const headerNav = document.querySelector(".header__navM");
 toggleMenu.addEventListener("click", () => {
   toggleMenu.classList.toggle("open");
   headerNav.classList.toggle("open");
 });
+
+
